8aead885bd84cd00c4896fbfdb11500ebd31f31f
DAbdulRaheem <raheemabduld56@gmail.com>
2025-11-03 13:11:31 +0530

    updated database

--- crud/env_pro/env_pro/settings.py
+++ crud/env_pro/env_pro/settings.py
@@ -122,18 +122,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'NAME': 'env_pro_db',
+        'USER': 'root',
+        'PASSWORD': 'Abdul@22',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
