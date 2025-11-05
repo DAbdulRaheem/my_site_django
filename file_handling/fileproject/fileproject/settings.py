e92d4ad241da8fb75541c9ce60132fe65c01349b
DAbdulRaheem <raheemabduld56@gmail.com>
2025-11-05 22:49:50 +0530

    deployment

--- file_handling/fileproject/fileproject/settings.py
+++ file_handling/fileproject/fileproject/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from pathlib import Path
+from dotenv import load_dotenv
+import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from dotenv import load_dotenv
